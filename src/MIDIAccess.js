e3509fc146223735657ca3e51971af778f58ba53
astridleighton <astridleighton@gmail.com>
2023-05-01 13:57:29 -0700

    added list output functionality
    lists available outs to console

--- src/MIDIAccess.js
+++ src/MIDIAccess.js
@@ -4,34 +4,50 @@ import App from './App.js';
 
 class MIDIAccess
 {
+
+    
+
+
     constructor()
     {
         navigator.requestMIDIAccess()
             .then(this.onMIDISuccess.bind(this), this.onMIDIFailure.bind(this));
+        this.midi = null;
     }
 
     onMIDISuccess(midiAccess)
     {
         console.log("WebMIDI is supported in browser.");
+        this.midi = midiAccess;
+
         var midiIns = midiAccess.inputs;
         var midiOuts = midiAccess.outputs;
 
         const inputs = midiIns.values();
+        const outputs = midiOuts.values();
 
         // list midi inputs to console
         if (inputs != null)
         {
             for(let input of inputs)
             {
-                console.log(`Found MIDI input: ${input.name}`);
-                input.onMIDIMessage = this.onMIDIMessage.bind(this);
+                console.log(`Found MIDI input: ${input.name}, ID: ${input.id}`);
+                //input.onMIDIMessage = this.onMIDIMessage.bind(this);
             }
         }
         else
         {
             console.log("Please connect MIDI device.");
         }
-        
+
+        // list midi outputs to console
+        if(outputs != null)
+        {
+            for(let output of outputs)
+            {
+                console.log(`Found MIDI output: ${output.name}, ID: ${output.id}`);
+            }
+        }
     }
 
     onMIDIFailure =  () =>
@@ -39,10 +55,16 @@ class MIDIAccess
         console.log("Midi failure.");
     }
 
-    getMIDIInfo = (event) =>
+    onMIDIMessage = (event) =>
     {
-        const note = event.data[1];
-        console.log(`Playing note: ${note}`);
+        let message = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
+
+        for(const character of event.data)
+        {
+            message += `0x${character.toString(16)}`;
+        }
+
+        console.log(message);
     }
 }
 

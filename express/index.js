b750fba868f8383c1f365255afa62fe95838d793
astridleighton <astridleighton@gmail.com>
2023-09-20 12:04:22 -0700

    got project back up and running

--- express/index.js
+++ express/index.js
@@ -1,9 +1,9 @@
-const { DAO } = require('./lib/app/database/dao.js');
+//const { DAO } = require('./lib/app/database/dao.js');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+/*const cors = require('cors');
 
-app.use(cors());
+app.use(cors());*/
 
 // set up database
 const dbHost = "localhost"
